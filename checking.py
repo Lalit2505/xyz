e2408d5814a04b2389f2bc8b4ae47c609fe374df
Lalit2505 <lalitsharnagat250597@gmail.com>
2023-01-18 23:16:38 +0530

    first commit

--- checking.py
+++
@@ -1,9 +0,0 @@
-from pdf import converter
-
-pdf_file='setu.pdf'
-docx_file='setu_1.docx'
-
-cv=converter(pdf_file)
-cv.convert(docx_file)
-cv.close()
-
--- datefunction.py
+++
@@ -1,5 +0,0 @@
-import datetime
-x=datetime.datetime.now()
-m=x.strftime("%y")
-print(x)
-print(m)
